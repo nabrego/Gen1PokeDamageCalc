61730d6d148fdba23036cb20b3479ca80a89c4f5
nabrego <93846936+nabrego@users.noreply.github.com>
2024-10-01 11:36:46 -0700

    Setup basic front and back end

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,34 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require('./models/Users');
+const cors = require("cors");
 
-mongoose.connect("mongodb+srv://nickabrego1:qt96H7b45faYGRFd@pokemon.ldci5.mongodb.net/")
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://nickabrego1:qt96H7b45faYGRFd@pokemon.ldci5.mongodb.net/Pokemon?retryWrites=true&w=majority&appName=Pokemon");
+
+app.get("/getUsers", (req, res) =>{
+    UserModel.find({})
+    .then(docs => {
+        console.log(docs);
+        res.json(docs);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    });
+});
+
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+});
 
 app.listen(3001, () => {
-    console.log("Server Runs Perfectly");
+    console.log("Server runs perfectly!");
 });
