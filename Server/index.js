6110e32c3127fe9cb3945a2e3e6130ff3419a922
nabrego <93846936+nabrego@users.noreply.github.com>
2024-10-06 12:32:32 -0700

    fixed backend

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const damCalcRoute = require("./routes/damCalcRoute");
 
+const app = express();
+
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ["https://gen1-poke-damage-calc-ro5w.vercel.app"],
-        methods: ["POST", "GET"]
-    }
-));
+app.use(cors({
+    origin: ["https://gen1-poke-damage-calc-ro5w.vercel.app"],
+    methods: ["POST", "GET"]
+}));
+
+app.get("/", (req, res) => {
+    res.send("API is running!");
+});
+
 app.use('/', damCalcRoute);
 
 module.exports = app;
